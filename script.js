bcb83a8fa1422d1b114a1478ec2af70ccc94d52a
Muhammad Saifullah Khalid <490558+saifee@users.noreply.github.com>
2024-07-02 12:42:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let slideIndex = 0;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
     const slides = document.querySelectorAll('.slide');
-    for (let i = 0; i < slides.length; i++) {
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = 'none';
     }
-    slideIndex++;
-    if (slide
+    slides[slideIndex-1].style.display = 'block';
+}
 
+setInterval(function() {
+    plusSlides(1);
+}, 5000); // Change image every 5 seconds
