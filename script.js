669ce4867b1a955f510fad50a9b49e4f1834ab42
Muhammad Saifullah Khalid <490558+saifee@users.noreply.github.com>
2024-07-02 12:36:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.querySelectorAll('.slide');
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slideIndex++;
+    if (slide
 
